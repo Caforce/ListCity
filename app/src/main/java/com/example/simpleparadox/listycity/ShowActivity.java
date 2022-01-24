8334d68edfe7a04da839d79c12615ef8d994cc40
Mahfuzul Haque <mahfuz15.ca@gmail.com>
2022-01-24 23:53:31 +0600

    First Commit

---
+++ app/src/main/java/com/example/simpleparadox/listycity/ShowActivity.java
@@ -0,0 +1,42 @@
+package com.example.simpleparadox.listycity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ShowActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.showactivity);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.backbutton);
+        Bundle cityName = getIntent().getExtras();
+        textView.setText(cityName.getString("name"));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowActivity.this.finish();
+
+            }
+        });
+    }
+}
